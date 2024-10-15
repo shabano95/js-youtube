6e3802ff07620e7d859cf039df4dae00d30aeef9
shabano95 <talktoa.bano@gmail.com>
2024-10-15 07:25:25 +0000

    container config done

---
+++ 01_basics/variable.js
@@ -0,0 +1,25 @@
+const accountId = 122234
+let accountEmail = 'hira@google.com'
+var accountPassword = '123334'
+accountcity = 'lahore'
+
+accountcity = 'karachi'
+accountEmail = 'khusha@gmailcom'
+accountPassword = '87654'
+/*
+prefer not to use var frequently instead use let and const
+bcz of issue in block scope and functinal scope
+*/
+// accountId = 44324 //not allowed to use value cannt be changed
+// alert (3+3)we are using node, not browser thats why get error
+console.table([accountcity,accountEmail ,accountPassword ,accountId ]);
+// perimitive data types
+/*
+string = "khushal"
+number = 23455
+let accountno; {undefined} 
+boolean {true or false}
+null
+bigint
+symbol ==> unique values
+*/
