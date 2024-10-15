7b8610439b30175e471f7175c0ecefff201dc529
shabano95 <talktoa.bano@gmail.com>
2024-10-15 18:37:22 +0000

    type_cnversion operation

---
+++ 01_basics/04_typeconversion.js
@@ -0,0 +1,19 @@
+/* changing value of data from one type to another is type conversion
+01_impicit==>js change datatype itsef when  it necessery
+02_expicit==> we do it menually 
+*/
+// let var1 = 'hello'; //string
+// let var2 = 22 //no
+// console.log(var1 + var2); //return hello22
+// console.log(typeof (var1 + var2)); //string
+
+//console.log(true + 22); //boolean value convert true into 1 then add in 22 which will be =23
+//console.log(22 +false); //false =0 so value erturn 22
+console.log(undefined +true);
+console.log(typeof (null +11));
+
+//implicit conversion
+console.log("khushal" + 7); //khushal7 (it c)
+console.log(5 + "khushal");
+
+
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,43 @@
+// 01_primitive datatypes
+/*
+number
+boolean
+string
+undefuned
+null
+bigint
+symbol
+*/
+// Numbers
+let num1 = 12 //integer
+let num2 = 13.2 //floating-point
+console.log(typeof num1);
+console.log(typeof num2);
+//infinity,negative infinity & not a number
+console.log(10/0); //when any no divide by 0 it return infinite value
+console.log(-3/0);
+console.log('ábc'*2); //wron mathematical operation
+
+
+// string
+let name1 = 'basha';
+let name2= "sha";
+let name3= `hello,${name1}!`;
+console.log([name1,name2,name3]);
+console.log(typeof[name1,name2,name3]);
+console.log(typeof name1);
+
+// boolean
+let isdefined =true
+let iscorrect = false
+console.log(isdefined);
+console.log(iscorrect);
+
+console.log(typeof isdefined);
+
+
+
+// 02_non primitive datatypes
+/*objects
+(arrays & functions)
+*/
