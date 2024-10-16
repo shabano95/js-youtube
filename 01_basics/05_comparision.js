166ced23bd115b1ddb81aaf3622f651974e509ac
shabano95 <talktoa.bano@gmail.com>
2024-10-16 14:01:43 +0000

    container config done

---
+++ 01_basics/05_comparision.js
@@ -0,0 +1,27 @@
+// its a common thing ,no problem would  evolve when use same kind of data
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 >= 1);
+// console.log(2 !=0);
+// console.log(2 ==0);
+
+// using different  kind of datatypes in comparision 
+// console.log("2" >= 0);
+// console.log("2" <= 0);
+
+// in case of null
+console.log(null > 0);
+console.log(null < 0);
+console.log(null <= 0);
+console.log(null >= 0);
+console.log(null != 0);
+console.log(null == 0);
+
+
+console.log(typeof (null > 0));
+console.log(typeof (null < 0));
+console.log(typeof (null <= 0));
+console.log(typeof (null >= 0));
+console.log(typeof (null != 0));
+console.log(typeof (null == 0));
